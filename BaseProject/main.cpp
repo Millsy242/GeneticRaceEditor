e80351130a9b2d6c9ce1353c2d3ac30dec8da5f1
Daniel Harvey <dhmillsy6@gmail.com>
2019-06-15 15:28:35 +0100

    Initial Commit

---
+++ BaseProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BaseProject
+//
+//  Created by Daniel Harvey on 15/06/2019.
+//  Copyright © 2019 Daniel Harvey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
